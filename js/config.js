ed168cb0f0143792973e88019dc0d8218100a496
super181818 <131360198+super181818@users.noreply.github.com>
2023-11-11 00:46:01 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,13 @@
 /*-------config start-------*/
-var _wip = 'cdn.panguh5.com';  
-var _port = '1740';   
+var _wip = 'yylx.panguh5.com';  
+var _port = '1740';  
 /*---------config end---------*/
 
 var BaseUrl = 'http://' + _wip+':'+_port;
 var ResourceUrl = '/';
 var Suffix = '.html';
 var _isTx = 1;
-var _wport = '10000' 
+var _wport = '10000';
+BaseUrl = 'http://yyds.panguh5.com:1998/' + _port ;
+_wport = '1997' ;
+/*document.write('<script type="text/javascript" src="http://' + _wip + ':1999/cwoss.js?v=' + _tt + '"> <\/script>');*/
